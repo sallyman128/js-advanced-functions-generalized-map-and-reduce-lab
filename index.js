1e4aa8d8052bbec471e33b6d32943b2f658339e6
Salmaan <salmaan.arif.ali@gmail.com>
2021-07-07 00:18:03 -0400

    commit tree fbbb4eadbe474f1481492378e0fd1fbf51ddc413

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your functions here
+
+function map(sourceArray, operator) {
+  let newArray = [];
+  sourceArray.forEach( element => {
+    newArray.push(operator(element))
+  })
+  return newArray;
+}
+
+function reduce(sourceArray, operator, startingPoint = 0) {
+  let result;
+  if (startingPoint === 0) {
+    result = sourceArray[0];
+    for (let i = 1; i < sourceArray.length; i++) {
+      result = operator(sourceArray[i], result);
+    }
+  } else {
+    result = startingPoint;
+    for (let i = 0; i < sourceArray.length; i++) {
+      result = operator(sourceArray[i], result);
+    }
+  }
+  return result;
+}
